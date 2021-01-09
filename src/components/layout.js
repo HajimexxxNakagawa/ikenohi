4684ce7277617089b4f9b4ffbd04aebdb421a2bf
Hajime Nakagawa <navyblue0914@icloud.com>
2021-01-09 17:31:32 +0900

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,23 +4,17 @@ import base from './base.css'
 import Container from './container'
 import Navigation from './navigation'
 
-class Template extends React.Component {
-  render() {
-    const { location, children } = this.props
-    let header
-
-    let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
-
-    return (
-      <Container>
-        <Navigation />
-        {children}
-      </Container>
-    )
+const Template = ({ location, children }) => {
+  if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+    rootPath = __PATH_PREFIX__ + `/`
   }
+
+  return (
+    <Container>
+      <Navigation />
+      {children}
+    </Container>
+  )
 }
 
 export default Template
