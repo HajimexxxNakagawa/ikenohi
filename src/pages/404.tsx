8b3517d5b151e48feea84c6128a71327b1f3e124
Hajime Nakagawa <navyblue0914@icloud.com>
2021-02-16 12:57:52 +0900

    Link fine tuning

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { ILink } from '../utils/ILink'
+import { ColoredILink } from '../utils/ILink'
 
 const NotFound = () => (
   <Layout>
     <div style={{ height: '60vh', paddingTop: '24vh', textAlign: 'center' }}>
       <h1>Sorry...</h1>
       <p>お探しのページは見つかりませんでした</p>
-      <ILink to="/">Topへ戻る</ILink>
+      <ColoredILink to="/">Topへ戻る</ColoredILink>
     </div>
   </Layout>
 )
