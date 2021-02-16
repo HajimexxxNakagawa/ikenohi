e87b6a8d754a06e739020281a1576a21840de5e9
Hajime Nakagawa <navyblue0914@icloud.com>
2021-02-16 12:52:08 +0900

    404 page fine tuning

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { ILink } from '../utils/ILink'
 
 const NotFound = () => (
   <Layout>
-    <h1>Sorry...</h1>
-    <p>お探しのページは見つかりませんでした。</p>
+    <div style={{ height: '60vh', paddingTop: '24vh', textAlign: 'center' }}>
+      <h1>Sorry...</h1>
+      <p>お探しのページは見つかりませんでした</p>
+      <ILink to="/">Topへ戻る</ILink>
+    </div>
   </Layout>
 )
 
--- src/utils/ILink.tsx
+++ src/utils/ILink.tsx
@@ -3,4 +3,9 @@ import styled from 'styled-components'
 
 export const ILink = styled(Link)`
   text-decoration: none;
+  transition: 0.3s;
+  color: #2b98b0;
+  &:hover {
+    color: #54c5de;
+  }
 `
