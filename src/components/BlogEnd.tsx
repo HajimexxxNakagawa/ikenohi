ffb0cef0e429dfa53e8dbc86b400168c338b8585
Hajime Nakagawa <navyblue0914@icloud.com>
2021-02-17 16:51:10 +0900

    fine tuning

--- src/components/BlogEnd.tsx
+++ src/components/BlogEnd.tsx
@@ -15,7 +15,7 @@ const BlogEnd = ({ tags }: BlogEndProps) => (
       {tags && tags.map((tag) => <Tag label={tag} key={tag} />)}
     </TagArea>
     <p style={{ textAlign: 'center' }}>
-      <ILink to="/">Topへ</ILink>
+      <ILink to="/">Homeへ</ILink>
     </p>
     {/* about me */}
   </BlogEndBase>
@@ -25,6 +25,9 @@ export default BlogEnd
 
 const BlogEndBase = styled.div`
   margin: 3rem auto;
+  border-top: 1px solid #333;
+  width: 90%;
+  max-width: 800px;
 `
 
 const TagArea = styled.div`
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ const NotFound = () => (
     <div style={{ height: '60vh', paddingTop: '24vh', textAlign: 'center' }}>
       <h1>Sorry...</h1>
       <p>お探しのページは見つかりませんでした</p>
-      <ColoredILink to="/">Topへ戻る</ColoredILink>
+      <ColoredILink to="/">Homeへ戻る</ColoredILink>
     </div>
   </Layout>
 )
